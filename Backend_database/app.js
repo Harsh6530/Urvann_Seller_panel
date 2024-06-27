e67e704d9bd85ca68fc2c5eb1af4ad57becb6ddb
Harsh Sharma <119579048+Harsh6530@users.noreply.github.com>
2024-06-27 14:10:08 +0530

    commit

--- Backend_database/app.js
+++ Backend_database/app.js
@@ -11,8 +11,6 @@ const Route = require('./models/route'); // Import the Route model
 app.use(express.json());
 app.use(cors()); // Enable CORS for all routes
 
-console.log("success")
-
 // MongoDB connection URI
 const MONGODB_URI = 'mongodb+srv://sambhav:UrvannGenie01@urvanngenie.u7r4o.mongodb.net/UrvannSellerApp?retryWrites=true&w=majority&appName=UrvannGenie';
 
@@ -164,5 +162,5 @@ app.get('/api/products', async (req, res) => {
 });
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
