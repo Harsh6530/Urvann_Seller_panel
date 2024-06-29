0629b7f28f0b14d99416490ef62da19bbcc49e63
Harsh Sharma <119579048+Harsh6530@users.noreply.github.com>
2024-06-30 00:11:31 +0530

    commit

--- server.js
+++ server.js
@@ -161,5 +161,5 @@ app.get('/api/products', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 443;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
