f603acb7d3a796d5e7f21bca8d895b0701d9c808
Nan211704 <100822323+Nan211704@users.noreply.github.com>
2024-07-02 01:13:44 +0530

    commit

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,6 +1,6 @@
 import { API_BASE_URL } from '@env';
 import React, { useState } from 'react';
-import { StyleSheet, Text, TextInput, View, TouchableOpacity, Alert, Image } from 'react-native';
+import { StyleSheet, Text, TextInput, View, TouchableOpacity, Alert, Image, ScrollView, KeyboardAvoidingView } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import axios from 'axios';
 
@@ -10,12 +10,11 @@ const LoginScreen = ({ navigation }) => {
 
   const handleLogin = async () => {
     try {
-      const response = await axios.post(`https://urvann-seller-panel-yc3k.onrender.com/api/login`, { username, password });
+      const response = await axios.post(`${API_BASE_URL}/api/login`, { username, password });
       if (response.status === 200 && response.data.token) {
         Alert.alert('Login successful', `Welcome, ${username}!`);
         navigation.navigate('RiderCodes', { sellerName: username });
       }
-
     } catch (error) {
       if (error.response && error.response.status === 401) {
         Alert.alert('Login failed', 'Invalid credentials');
@@ -28,34 +27,38 @@ const LoginScreen = ({ navigation }) => {
 
   return (
     <LinearGradient colors={['#fff', '#fff']} style={styles.container}>
-      <Image source={require('../assets/urvann.png')} style={styles.logo} />
-      <View style={styles.innerContainer}>
-        <Text style={styles.title}>Seller login</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Username"
-          placeholderTextColor="#888"
-          value={username}
-          onChangeText={(text) => setUsername(text.toUpperCase())}
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Password"
-          placeholderTextColor="#888"
-          value={password}
-          onChangeText={setPassword}
-          secureTextEntry
-        />
-        <TouchableOpacity style={styles.button} onPress={handleLogin}>
-          <Text style={styles.buttonText}>Login</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={[styles.button, styles.registerButton]} onPress={() => navigation.navigate('Register')}>
-          <Text style={[styles.buttonText, styles.registerButtonText]}>New user? Register</Text>
-        </TouchableOpacity>
-        {/* <TouchableOpacity style={styles.forgotButton} onPress={() => navigation.navigate('ForgotPassword')}>
-          <Text style={styles.forgotButtonText}>Forgot Password?</Text>
-        </TouchableOpacity> */}
-      </View>
+      <ScrollView contentContainerStyle={{ flexGrow: 1 }} keyboardShouldPersistTaps="handled">
+        <KeyboardAvoidingView behavior="padding" style={styles.container}>
+          <Image source={require('../assets/urvann.png')} style={styles.logo} />
+          <View style={styles.innerContainer}>
+            <Text style={styles.title}>Seller login</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Username"
+              placeholderTextColor="#888"
+              value={username}
+              onChangeText={(text) => setUsername(text.toUpperCase())}
+            />
+            <TextInput
+              style={styles.input}
+              placeholder="Password"
+              placeholderTextColor="#888"
+              value={password}
+              onChangeText={setPassword}
+              secureTextEntry
+            />
+            <TouchableOpacity style={styles.button} onPress={handleLogin}>
+              <Text style={styles.buttonText}>Login</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={[styles.button, styles.registerButton]} onPress={() => navigation.navigate('Register')}>
+              <Text style={[styles.buttonText, styles.registerButtonText]}>New user? Register</Text>
+            </TouchableOpacity>
+            {/* <TouchableOpacity style={styles.forgotButton} onPress={() => navigation.navigate('ForgotPassword')}>
+              <Text style={styles.forgotButtonText}>Forgot Password?</Text>
+            </TouchableOpacity> */}
+          </View>
+        </KeyboardAvoidingView>
+      </ScrollView>
     </LinearGradient>
   );
 };
