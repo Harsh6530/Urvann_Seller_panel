22b7a8f9ca39863723da5f96e4c9930fc3126863
Nan211704 <100822323+Nan211704@users.noreply.github.com>
2024-11-13 15:52:52 +0530

    commit

--- models/route.js
+++ models/route.js
@@ -24,7 +24,7 @@ const routeSchema = new mongoose.Schema({
   Pickup_Status: { type: String, required: true },
   metafield_delivery_status: { type: String, required: true },
   metafield_order_type: { type: String, required: true },
-  bin: { type: String, required: true}
+  bin: { type: Number, required: true}
 }, { collection: 'route' });
 
 module.exports = mongoose.model('Route', routeSchema);
