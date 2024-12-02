8a620bd9dd5b568e12f6bd4071b341a3b13b1c16
Samya Saha <77977204+Samya-S@users.noreply.github.com>
2024-12-02 14:28:57 +0530

    Refactor user authentication: move register and login logic to user controller and use environment variable for JWT secret

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const { Types } = mongoose; 
-const jwt = require('jsonwebtoken');
 const cors = require('cors');
 const app = express();
 require("dotenv").config();
 
 const connectToDB = require('./middlewares/connectToDB');
 
+const user = require('./controllers/user');
+
 const Photo = require('./models/photo');
-const User = require('./models/userDetails');
 const DeliveryUpdate = require('./models/deliveryUpdate');
 const Summary = require('./models/Summary');
 const Payable = require('./models/Payable');
@@ -24,61 +24,11 @@ app.use(cors());
 // Connect to MongoDB
 connectToDB();
 
-// Hardcoded JWT secret key (use this only for development/testing)
-const JWT_SECRET = 'your_secret_key'; // Replace 'your_secret_key' with a strong secret key
-
 // Register route
-app.post('/api/register', async (req, res) => {
-  const { username, password } = req.body;
-
-  try {
-    // Check if user already exists
-    let user = await User.findOne({ username });
-    if (user) {
-      return res.status(400).json({ message: 'User already exists' });
-    }
-
-    // Create new user
-    user = new User({
-      username,
-      password, // Store the password as it is
-    });
-
-    // Save user to database
-    await user.save();
-
-    res.status(201).json({ message: 'User registered successfully' });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Internal server error' });
-  }
-});
+app.post('/api/register', user.registerUser);
 
 // Login route
-app.post('/api/login', async (req, res) => {
-  const { username, password } = req.body;
-
-    try {
-        // Check if user exists
-        const user = await User.findOne({ username });
-        if (!user) {
-            return res.status(404).json({ message: 'Driver not found' });
-        }
-
-        // Check if password matches
-        if (password !== user.password) {
-            return res.status(401).json({ message: 'Invalid credentials' });
-        }
-
-        // Generate JWT token
-        const token = jwt.sign({ userId: user._id }, JWT_SECRET, { expiresIn: '1h' });
-
-        res.status(200).json({ token });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Internal server error' });
-    }
-});
+app.post('/api/login', user.loginUser);
 
 
 // GET /api/sellers
